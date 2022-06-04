0367cda41bbecda989e77464d0df411d23fcbc18
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2022-06-04 15:30:49 +0600

    Categories app, model and migration

--- graphql_blog/settings.py
+++ graphql_blog/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "graphene_django"
+    "graphene_django",
+    "categories"
 ]
 
 MIDDLEWARE = [
@@ -116,3 +117,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GRAPHENE = {
+    "SCHEMA": "graphql_blog.schema.schema"
+}
